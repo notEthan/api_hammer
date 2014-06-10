360b13a4ef727001a5574f9ce91aa947cf3c0043
Ethan <ethan@unth>
2014-06-10 13:48:01 -0400

    one active_record_find_with_caching_test

---
+++ test/active_record_cache_find_by_test.rb
@@ -0,0 +1,41 @@
+proc { |p| $:.unshift(p) unless $:.any? { |lp| File.expand_path(lp) == p } }.call(File.expand_path('.', File.dirname(__FILE__)))
+require 'helper'
+
+require 'active_support/cache'
+require 'active_record'
+require 'api_hammer/active_record_cache_find_by'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database  => ":memory:"
+)
+
+ActiveRecord::Schema.define do
+  create_table :albums do |table|
+    table.column :title, :string
+    table.column :performer, :string
+  end
+end
+
+class Album < ActiveRecord::Base
+  cache_find_by(:id)
+  cache_find_by(:title)
+end
+
+module Rails
+  class << self
+    def cache
+      @cache ||= ActiveSupport::Cache::MemoryStore.new
+    end
+  end
+end
+
+describe 'ActiveRecord::Base.cache_find_by' do
+  it('caches') do
+    Album.create!(:title => 'x')
+    key = "cache_find_by/albums/title/x"
+    assert !Rails.cache.read(key)
+    assert Album.find_by_title('x')
+    assert Rails.cache.read(key)
+  end
+end
