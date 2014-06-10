9a78ed744dd4671a6434cef40a0c1b28ad528fdc
Ethan <ethan@unth>
2014-06-10 18:43:29 -0400

    active_record_find_with_caching_tests

--- test/active_record_cache_find_by_test.rb
+++ test/active_record_cache_find_by_test.rb
@@ -20,6 +20,7 @@ end
 class Album < ActiveRecord::Base
   cache_find_by(:id)
   cache_find_by(:title)
+  cache_find_by(:title, :performer)
 end
 
 module Rails
@@ -45,8 +46,28 @@ describe 'ActiveRecord::Base.cache_find_by' do
     assert !Rails.cache.read(key)
   end
 
-  it('caches') do
+  after do
+    Album.destroy_all
+    Rails.cache.clear
+  end
+
+  it('caches #find by primary key') do
+    id = Album.create!(:title => 'x').id
+    assert_caches("cache_find_by/albums/id/#{id}") { assert Album.find(id) }
+  end
+
+  it('caches with one attribute') do
     Album.create!(:title => 'x')
     assert_caches("cache_find_by/albums/title/x") { assert Album.find_by_title('x') }
+  end
+
+  it('does not cache with one attribute') do
+    Album.create!(:performer => 'x')
+    assert_not_caches("cache_find_by/albums/performer/x") { assert Album.find_by_performer('x') }
+  end
+
+  it('caches with two attributes') do
+    Album.create!(:title => 'x', :performer => 'y')
+    assert_caches("cache_find_by/albums/performer/y/title/x") { assert Album.find_by_title_and_performer('x', 'y') }
   end
 end
