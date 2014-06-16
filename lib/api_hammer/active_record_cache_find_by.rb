6a390c5fd7576aa81e878a822c3ccbb71143d96b
Ethan <ethan@unth>
2014-06-16 02:15:52 -0400

    make the sets of attributes to cache by inheritable, call them cache_find_bys

--- lib/api_hammer/active_record_cache_find_by.rb
+++ lib/api_hammer/active_record_cache_find_by.rb
@@ -27,11 +27,11 @@ module ActiveRecord
           where_value.right
         end
       end
-      cache_find_by = klass.instance_eval { @cache_find_by }
-      can_cache &&= cache_find_by &&
+      cache_find_bys = klass.cache_find_bys
+      can_cache &&= cache_find_bys &&
         !loaded? && # if it's loaded no need to hit cache 
         where_values.all? { |wv| wv.is_a?(Arel::Nodes::Equality) } && # no inequality or that sort of thing 
-        cache_find_by.include?(where_values.map { |wv| wv.left.name }.sort) && # any of the set of where-values to cache match this relation 
+        cache_find_bys.include?(where_values.map { |wv| wv.left.name }.sort) && # any of the set of where-values to cache match this relation 
         where_values.map(&actual_right).all? { |r| r.is_a?(String) || r.is_a?(Numeric) } && # check all right side values are simple types, number or string 
         offset_value.nil? &&
         joins_values.blank? &&
@@ -70,6 +70,14 @@ module ActiveRecord
         # dummy; this gets set below 
       end
 
+      def cache_find_bys=(val)
+        define_singleton_method(:cache_find_bys) { val }
+      end
+
+      def cache_find_bys
+        nil
+      end
+
       # causes requests to retrieve a record by the given attributes (all of them) to be cached. 
       # this is for single records only. it is unsafe to use with a set of attributes whose values 
       # (in conjunction) may be associated with multiple records. 
@@ -82,12 +90,12 @@ module ActiveRecord
       # beware of multiple application servers with different caches - a record cached in multiple will not 
       # be invalidated in all when it is saved in one.
       def cache_find_by(*attribute_names)
-        unless @cache_find_by
+        unless cache_find_bys
           # initial setup
-          @cache_find_by = Set.new
+          self.cache_find_bys = Set.new
           after_update :flush_find_cache
         end
-        @cache_find_by << attribute_names.map { |n| n.is_a?(Symbol) ? n.to_s : n.is_a?(String) ? n : raise(ArgumentError) }.sort
+        self.cache_find_bys |= [attribute_names.map { |n| n.is_a?(Symbol) ? n.to_s : n.is_a?(String) ? n : raise(ArgumentError) }.sort]
       end
     end
 
@@ -97,7 +105,7 @@ module ActiveRecord
 
     # clears this record from the cache used by cache_find_by
     def flush_find_cache
-      self.class.instance_eval { @cache_find_by }.each do |attribute_names|
+      self.class.cache_find_bys.each do |attribute_names|
         cache_key_prefix = ['cache_find_by', self.class.table_name]
         find_attributes = attribute_names.map { |attr_name| [attr_name, self.send(:attribute_was, attr_name)] }.inject([], &:+)
         cache_key = (cache_key_prefix + find_attributes).join('/')
