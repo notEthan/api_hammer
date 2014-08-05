977fe29ec3136cc99e21efbad2f412559d66f752
Ethan <ethan@unth>
2014-08-04 21:20:08 -0400

    RailsOrSidekiqLogger

--- lib/api_hammer.rb
+++ lib/api_hammer.rb
@@ -6,4 +6,6 @@ module ApiHammer
   autoload :ShowTextExceptions, 'api_hammer/show_text_exceptions.rb'
   autoload :TrailingNewline, 'api_hammer/trailing_newline.rb'
   autoload :Weblink, 'api_hammer/weblink.rb'
+  autoload :RailsOrSidekiqLoggerMiddleware, 'api_hammer/rails_or_sidekiq_logger'
+  autoload :RailsOrSidekiqLogger, 'api_hammer/rails_or_sidekiq_logger'
 end
---
+++ lib/api_hammer/rails_or_sidekiq_logger.rb
@@ -0,0 +1,43 @@
+# switch a logger between Rails.logger and sidekiq's logger 
+#
+#     Sidekiq.configure_server do |config|
+#       ...
+#       
+#       config.server_middleware do |chain|
+#         chain.add ApiHammer::RailsOrSidekiqLoggerMiddleware
+#       end
+#     end
+#
+# and in a class 
+#
+#     class Foo
+#       include ApiHammer::RailsOrSidekiqLogger
+#     end
+#
+module ApiHammer
+  class RailsOrSidekiqLoggerMiddleware
+    def call(worker, msg, queue, &block)
+      ApiHammer::RailsOrSidekiqLogger.with_logger(worker.logger, &block)
+    end
+  end
+
+  module RailsOrSidekiqLogger
+    LOGGER_KEY = 'api_hammer_rails_or_sidekiq_logger'
+    def with_logger(logger)
+      orig_logger = Thread.current[LOGGER_KEY]
+      begin
+        Thread.current[LOGGER_KEY] = logger
+        yield
+      ensure
+        Thread.current[LOGGER_KEY] = orig_logger
+      end
+    end
+
+    def logger
+      logger = Thread.current[LOGGER_KEY] || ::Rails.logger
+      logger.respond_to?(:tagged) ? logger : ::ActiveSupport::TaggedLogging.new(logger)
+    end
+
+    extend self
+  end
+end
