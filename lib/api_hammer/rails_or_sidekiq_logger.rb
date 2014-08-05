7c849f13e391fd9a6223e4faacdc7008613d34b4
Ethan <ethan@unth>
2014-08-05 01:30:56 -0400

    move documentation to the right place in rails/sidekiq logger

--- lib/api_hammer/rails_or_sidekiq_logger.rb
+++ lib/api_hammer/rails_or_sidekiq_logger.rb
@@ -1,26 +1,26 @@
-# switch a logger between Rails.logger and sidekiq's logger 
-#
-#     Sidekiq.configure_server do |config|
-#       ...
-#       
-#       config.server_middleware do |chain|
-#         chain.add ApiHammer::RailsOrSidekiqLoggerMiddleware
-#       end
-#     end
-#
-# and in a class 
-#
-#     class Foo
-#       include ApiHammer::RailsOrSidekiqLogger
-#     end
-#
 module ApiHammer
+  # switch a logger between Rails.logger and sidekiq's logger 
+  #
+  #     Sidekiq.configure_server do |config|
+  #       ...
+  #       
+  #       config.server_middleware do |chain|
+  #         chain.add ApiHammer::RailsOrSidekiqLoggerMiddleware
+  #       end
+  #     end
   class RailsOrSidekiqLoggerMiddleware
     def call(worker, msg, queue, &block)
       ApiHammer::RailsOrSidekiqLogger.with_logger(worker.logger, &block)
     end
   end
 
+  # include in a class to define #logger which will switch between Sidekiq and Rails 
+  # using RailsOrSidekiqLoggerMiddleware
+  #
+  #     class Foo
+  #       include ApiHammer::RailsOrSidekiqLogger
+  #     end
+  #
   module RailsOrSidekiqLogger
     LOGGER_KEY = 'api_hammer_rails_or_sidekiq_logger'
     def with_logger(logger)
