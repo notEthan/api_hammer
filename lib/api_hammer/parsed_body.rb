72faeeeddbf2bb5c0d0c04e0ccd0c6740904707a
Ethan <ethan@unth>
2015-01-07 09:40:28 -0800

    move #object to less eager in ParsedBody

--- lib/api_hammer/parsed_body.rb
+++ lib/api_hammer/parsed_body.rb
@@ -2,13 +2,16 @@ require 'rack'
 
 module ApiHammer
   class ParsedBody
-    attr_reader :body, :content_type, :media_type, :object
+    attr_reader :body, :content_type, :media_type
 
     def initialize(body, content_type)
       @body = body
       @content_type = content_type
       @media_type = ::Rack::Request.new({'CONTENT_TYPE' => content_type}).media_type
-      @object = begin
+    end
+
+    def object
+      instance_variable_defined?(:@object) ? @object : @object = begin
         if media_type == 'application/json'
           JSON.parse(body) rescue nil
         elsif media_type == 'application/x-www-form-urlencoded'
