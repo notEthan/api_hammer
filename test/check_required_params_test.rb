9e8ee1e577d7d7a93257c620fdbfb1d4785f03b0
Ethan <ethan@unth>
2017-01-27 16:21:25 -0800

    add error_categories to tests

--- test/check_required_params_test.rb
+++ test/check_required_params_test.rb
@@ -31,49 +31,49 @@ require 'action_controller/metal/strong_parameters'
       it 'is missing id' do
         c = controller_with_params(:person => {:name => 'hammer', :height => '3'}, :lucky_numbers => ['2'])
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'id is required but was not provided', 'errors' => {'id' => ['id is required but was not provided']}}, err.body)
+        assert_equal({'error_message' => 'id is required but was not provided', 'errors' => {'id' => ['id is required but was not provided']}, 'error_categories' => ['MISSING_PARAMETERS']}, err.body)
       end
 
       it 'is missing person' do
         c = controller_with_params(:id => '99', :lucky_numbers => ['2'])
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'person is required but was not provided', 'errors' => {'person' => ['person is required but was not provided']}}, err.body)
+        assert_equal({'error_message' => 'person is required but was not provided', 'errors' => {'person' => ['person is required but was not provided']}, 'error_categories' => ['MISSING_PARAMETERS']}, err.body)
       end
 
       it 'is has the wrong type for person' do
         c = controller_with_params(:id => '99', :person => ['hammer', '3'], :lucky_numbers => ['2'])
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'person must be a Hash', 'errors' => {'person' => ['person must be a Hash']}}, err.body)
+        assert_equal({'error_message' => 'person must be a Hash', 'errors' => {'person' => ['person must be a Hash']}, 'error_categories' => ['INVALID_PARAMETERS']}, err.body)
       end
 
       it 'is has the wrong type for person with hash check' do
         c = controller_with_params(:person => [])
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(:person => {:id => Fixnum}) }
-        assert_equal({'error_message' => 'person must be a Hash', 'errors' => {'person' => ['person must be a Hash']}}, err.body)
+        assert_equal({'error_message' => 'person must be a Hash', 'errors' => {'person' => ['person must be a Hash']}, 'error_categories' => ['INVALID_PARAMETERS']}, err.body)
       end
 
       it 'is missing person#name' do
         c = controller_with_params(:id => '99', :person => {:height => '3'}, :lucky_numbers => ['2'])
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'person#name is required but was not provided', 'errors' => {'person#name' => ['person#name is required but was not provided']}}, err.body)
+        assert_equal({'error_message' => 'person#name is required but was not provided', 'errors' => {'person#name' => ['person#name is required but was not provided']}, 'error_categories' => ['MISSING_PARAMETERS']}, err.body)
       end
 
       it 'is missing lucky_numbers' do
         c = controller_with_params(:id => '99', :person => {:name => 'hammer', :height => '3'})
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'lucky_numbers is required but was not provided', 'errors' => {'lucky_numbers' => ['lucky_numbers is required but was not provided']}}, err.body)
+        assert_equal({'error_message' => 'lucky_numbers is required but was not provided', 'errors' => {'lucky_numbers' => ['lucky_numbers is required but was not provided']}, 'error_categories' => ['MISSING_PARAMETERS']}, err.body)
       end
 
       it 'has the wrong type for lucky_numbers' do
         c = controller_with_params(:id => '99', :person => {:name => 'hammer', :height => '3'}, :lucky_numbers => '2')
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'lucky_numbers must be a Array', 'errors' => {'lucky_numbers' => ['lucky_numbers must be a Array']}}, err.body)
+        assert_equal({'error_message' => 'lucky_numbers must be a Array', 'errors' => {'lucky_numbers' => ['lucky_numbers must be a Array']}, 'error_categories' => ['INVALID_PARAMETERS']}, err.body)
       end
 
       it 'has multiple problems' do
         c = controller_with_params({})
         err = assert_raises(ApiHammer::Rails::Halt) { c.check_required_params(checks) }
-        assert_equal({'error_message' => 'id is required but was not provided. person is required but was not provided. lucky_numbers is required but was not provided.', 'errors' => {'id' => ['id is required but was not provided'], 'person' => ['person is required but was not provided'], 'lucky_numbers' => ['lucky_numbers is required but was not provided']}}, err.body)
+        assert_equal({'error_message' => 'id is required but was not provided. person is required but was not provided. lucky_numbers is required but was not provided.', 'errors' => {'id' => ['id is required but was not provided'], 'person' => ['person is required but was not provided'], 'lucky_numbers' => ['lucky_numbers is required but was not provided']}, 'error_categories' => ['MISSING_PARAMETERS']}, err.body)
       end
     end
   end
