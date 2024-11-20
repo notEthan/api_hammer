9d5b1a01839529d4dacf3ee1e919833f226cd153
Ethan <ethan@unth.net>
2024-11-20 02:18:50 -0800

    rake gig

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,3 +5,20 @@ Rake::TestTask.new do |t|
   t.verbose = true
 end
 task 'default' => 'test'
+
+begin
+  require 'gig'
+  gig_loaded = true
+rescue LoadError
+end
+
+if gig_loaded
+  ignore_files = %w(
+    .github/**/*
+    .gitignore
+    Gemfile*
+    Rakefile.rb
+    test/**/*
+  ).map { |glob| Dir.glob(glob, File::FNM_DOTMATCH) }.inject([], &:|)
+  Gig.make_task(gemspec_filename: 'api_hammer.gemspec', ignore_files: ignore_files)
+end
